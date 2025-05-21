d1beb36d2f405b7326f76b34004f747e7f047829
Aydarus <aydarbar0769@gmail.com>
2025-05-21 09:43:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "filesystem.h"
 
+int main() {
+    FILE* fs = sahur_open("sahur.disk");
+    if (!fs) return 1;
+    
+    sahur_create(fs, "test.txt", "Hello Sahur!");
+    char* content = sahur_view(fs, "test.txt");
+    if (content) {
+        printf("Content: %s\n", content);
+        free(content);
+    }
+    
+    fclose(fs);
+    return 0;
+}
