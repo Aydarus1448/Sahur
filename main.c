1adc1536c413ac386fda18f1b3fdfde6014932e4
Aydarus <aydarbar0769@gmail.com>
2025-05-21 10:11:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,40 @@
 int main() {
     FILE* fs = sahur_open("sahur.disk");
     if (!fs) return 1;
+
+    char command[256];
+    char* args[3];
     
-    sahur_create(fs, "test.txt", "Hello Sahur!");
-    char* content = sahur_view(fs, "test.txt");
-    if (content) {
-        printf("Content: %s\n", content);
-        free(content);
+    while (1) {
+        printf("> ");
+        if (!fgets(command, sizeof(command), stdin)) break;
+        
+        command[strcspn(command, "\n")] = 0;
+        args[0] = strtok(command, " ");
+        args[1] = strtok(NULL, " ");
+        args[2] = strtok(NULL, "");
+        
+        if (!args[0]) continue;
+        
+        if (strcmp(args[0], "create") == 0 && args[1] && args[2]) {
+            sahur_create(fs, args[1], args[2]);
+        } 
+        else if (strcmp(args[0], "delete") == 0 && args[1]) {
+            sahur_delete(fs, args[1]);
+        }
+        else if (strcmp(args[0], "modify") == 0 && args[1] && args[2]) {
+            sahur_modify(fs, args[1], args[2]);
+        }
+        else if (strcmp(args[0], "view") == 0 && args[1]) {
+            char* content = sahur_view(fs, args[1]);
+            if (content) {
+                printf("%s\n", content);
+                free(content);
+            }
+        }
+        else if (strcmp(args[0], "exit") == 0) {
+            break;
+        }
     }
     
     fclose(fs);
